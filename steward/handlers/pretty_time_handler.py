de9eedcd8c4916ed8f3ae23dc301a5b65ae39d2e
KmXK <kir.guydo@gmail.com>
2025-03-10 01:18:52 +0300

    add pretty time deleting

--- steward/handlers/pretty_time_handler.py
+++ steward/handlers/pretty_time_handler.py
@@ -1,12 +1,20 @@
+import logging
+
 from steward.bot.context import ChatBotContext
 from steward.delayed_action.pretty_time import PrettyTimeDelayedAction
 from steward.handlers.command_handler import CommandHandler
 from steward.handlers.handler import Handler
 
+logger = logging.getLogger(__name__)
 
-@CommandHandler("pretty_time", only_admin=True)
+
+@CommandHandler("pretty_time", only_admin=True, arguments_template=r"(?P<action>.*)?")
 class PrettyTimeHandler(Handler):
-    async def chat(self, context: ChatBotContext):
+    async def chat(
+        self,
+        context: ChatBotContext,
+        action: str,
+    ):
         chat_id = context.message.chat_id
         delayed_action = next(
             filter(
@@ -17,6 +25,13 @@ class PrettyTimeHandler(Handler):
             ),
             None,
         )
+
+        if action == "delete":
+            logger.info("deleting: %s", delayed_action)
+            if delayed_action is not None:
+                self.repository.db.delayed_actions.remove(delayed_action)
+                await self.repository.save()
+            return True
 
         if delayed_action is None:
             self.repository.db.delayed_actions.append(
