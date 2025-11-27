83588cb295a79880d2477cbf75f47d5574a95d99
KmXK <guydo.k@yandex.by>
2025-11-28 02:49:52 +0300

    .

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -6,6 +6,14 @@ function App() {
   const [count, setCount] = useState(0)
   const [webApp, setWebApp] = useState(null)
 
+  return (
+    <>
+      <div>
+        Hello world!
+      </div>
+    </>
+  )
+
   useEffect(() => {
     // Инициализируем Telegram Web App
     init()
